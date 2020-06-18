dbe64bf4a36cbef04a4e87058d4b22f01e050a8a
Scala Steward <me@scala-steward.org>
2020-06-18 14:31:00 +0200

    Update slick-joda-mapper to 2.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
   val slickPg =  "com.github.tminglei" %% "slick-pg" % "0.19.0"
   val slickPgPlayJson = "com.github.tminglei" %% "slick-pg_play-json" % "0.19.0"
   val slickPgJts= "com.github.tminglei" %% "slick-pg_jts" % "0.19.0"
-  val slickJodaMapper = "com.github.tototoshi" %% "slick-joda-mapper" % "2.4.0"
+  val slickJodaMapper = "com.github.tototoshi" %% "slick-joda-mapper" % "2.4.2"
   val playJson = "com.typesafe.play" %% "play-json" % "2.8.1"
   val playJsonJoda = "com.typesafe.play" %% "play-json-joda" % "2.8.1"
   val slick = "com.typesafe.slick" %% "slick" % "3.3.2"
