7ab1febd9b8e2827298be5b323fe64b145feb81c
Tobias Jonas <t.jonas@innfactory.de>
2021-10-27 00:09:48 +0000

    Update joda-time to 2.10.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -30,7 +30,7 @@ object Dependencies {
   val slickCodegen    = "com.typesafe.slick"   %% "slick-codegen"      % "3.3.3"
   val slickHikaricp   = "com.typesafe.slick"   %% "slick-hikaricp"     % "3.3.3"
   val hikariCP        = "com.zaxxer"            % "HikariCP"           % "5.0.0"  exclude("org.slf4j", "slf4j-api")
-  val joda            = "joda-time"             % "joda-time"          % "2.10.12"
+  val joda            = "joda-time"             % "joda-time"          % "2.10.13"
   val postgresql      = "org.postgresql"        % "postgresql"         % "42.3.0"
   val cats = "org.typelevel" %% "cats-core" % "2.6.1"
   val henkanConvert =   "com.kailuowang" %% "henkan-convert" % "0.6.5"
