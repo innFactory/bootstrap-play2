358d666ff010e11f518459381ae92faff393e563
Alexander Hauenstein <a.hauenstein@innfactory.de>
2022-01-18 15:23:07 +0100

    feat: add showable

---
+++ app/de/innfactory/bootstrapplay2/commons/implicits/Showable.scala
@@ -0,0 +1,13 @@
+package de.innfactory.bootstrapplay2.commons.implicits
+
+trait Showable { this: Product =>
+
+  def show: String = {
+    val className = this.productPrefix
+    val fieldNames = this.productElementNames.toList
+    val fieldValues = this.productIterator.toList
+    val fields = fieldNames.zip(fieldValues).map { case (name, value) => s"$name = $value" }
+    fields.mkString(s"$className(", ", ", ")")
+  }
+  override def toString: String = show
+}
