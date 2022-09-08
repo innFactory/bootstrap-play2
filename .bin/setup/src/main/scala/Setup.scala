eca95e46ad2a527b4e14090b8afab39078a2c3cc
Alexander Hauenstein <a.hauenstein@innfactory.de>
2022-09-08 10:38:41 +0200

    feat: validate working directory

--- .bin/setup/src/main/scala/Setup.scala
+++ .bin/setup/src/main/scala/Setup.scala
@@ -3,18 +3,26 @@ import actions.help.Help
 import actions.packagedomain.PackageDomain
 import config.SetupConfig
 
+import java.nio.file.{Files, Paths}
+
 object Setup extends App {
 
+  println("start")
   implicit val config: SetupConfig = SetupConfig.get()
+  println(config)
 
-  args.length match {
-    case 0 =>
-      println("Missing argument!")
-      Help.referToHelp()
-    case 1 => handleArgs(args.head)
-    case _ =>
-      println("Too many arguments!")
-      Help.referToHelp()
+  workingDirectoryIsProjectRoot() match {
+    case Left(_) => println("Execute this script from your project root!")
+    case Right(_) =>
+      args.length match {
+        case 0 =>
+          println("Missing argument!")
+          Help.referToHelp()
+        case 1 => handleArgs(args.head)
+        case _ =>
+          println("Too many arguments!")
+          Help.referToHelp()
+      }
   }
 
   def handleArgs(argumentKey: String)(implicit config: SetupConfig): Unit =
@@ -29,4 +37,19 @@ object Setup extends App {
         println("Unsupported argument!")
         Help.referToHelp()
     }
+
+  def workingDirectoryIsProjectRoot()(implicit config: SetupConfig) = {
+    val filesInProjectRoot = Files.find(
+      Paths.get(System.getProperty("user.dir")),
+      0,
+      (path, basicFileAttributes) => {
+        println(path.getFileName.toFile.getName)
+        val fileName = path.getFileName.toFile.getName
+        fileName.matches(config.project.sourcesRoot) || fileName.matches(config.smithy.sourcesRoot)
+      }
+    )
+    if (filesInProjectRoot.count() == 2) Right(())
+    else Left(())
+  }
+
 }
