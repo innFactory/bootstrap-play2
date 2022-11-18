be916349fa55b2e578f4d48d50b066d0ac05f293
Tobias Jonas <t.jonas@innfactory.de>
2022-11-18 00:21:55 +0000

    chore: update slf4j-api from 2.0.3 to 2.0.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -51,7 +51,7 @@ object Dependencies {
   // If you want to use opencensus-scala inside an Akka HTTP project
   val opencensusAkkaHttp = "com.github.sebruck" %% "opencensus-scala-akka-http" % "0.7.2"
 
-  val sl4j = "org.slf4j" % "slf4j-api" % "2.0.3" intransitive
+  val sl4j = "org.slf4j" % "slf4j-api" % "2.0.4" intransitive
   val sharedDeps = "com.google.cloud" % "google-cloud-shared-dependencies" % "3.0.6"
   val logback = "ch.qos.logback" % "logback-classic" % "1.2.11"
   val logbackCore = "ch.qos.logback" % "logback-core" % "1.2.11"
