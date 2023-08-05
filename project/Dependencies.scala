18d5f948f41560a06beb9f5e1b52ad1b8b289a7f
Tobias Jonas <t.jonas@innfactory.de>
2023-08-05 00:19:21 +0000

    chore: update logback-classic, logback-core from 1.2.12 to 1.4.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -53,8 +53,8 @@ object Dependencies {
 
   val sl4j = "org.slf4j" % "slf4j-api" % "2.0.7" intransitive
   val sharedDeps = "com.google.cloud" % "google-cloud-shared-dependencies" % "3.14.0"
-  val logback = "ch.qos.logback" % "logback-classic" % "1.2.12"
-  val logbackCore = "ch.qos.logback" % "logback-core" % "1.2.12"
+  val logback = "ch.qos.logback" % "logback-classic" % "1.4.9"
+  val logbackCore = "ch.qos.logback" % "logback-core" % "1.4.9"
 
   lazy val list = Seq(
     scalaUtil,
