1e2f4676b6658bc29561456818a9cdb0f63a51ad
Alexander Hauenstein <a.hauenstein@innfactory.de>
2023-10-02 15:01:04 +0200

    fix: duplicate version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
   val slickCodegen = "com.typesafe.slick" %% "slick-codegen" % "3.3.3"
   val slickHikaricp = "com.typesafe.slick" %% "slick-hikaricp" % "3.3.3"
   val joda = "joda-time" % "joda-time" % "2.12.5"
-  val hikariCP = "com.zaxxer" % "HikariCP" % "5.0.1" % "5.0.1" exclude ("org.slf4j", "slf4j-api")
+  val hikariCP = "com.zaxxer" % "HikariCP" % "5.0.1" exclude ("org.slf4j", "slf4j-api")
   val postgresql = "org.postgresql" % "postgresql" % "42.6.0"
   val cats = "org.typelevel" %% "cats-core" % "2.10.0"
   val henkanConvert = "com.kailuowang" %% "henkan-convert" % "0.6.5"
