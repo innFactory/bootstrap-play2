88eedd0e670f8fe8d2bf78a789d58473817dd4ba
Tobias Jonas <t.jonas@innfactory.de>
2024-02-02 00:16:44 +0000

    chore: update google-cloud-shared-dependencies from 3.23.0 to 3.24.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
   val opencensusAkkaHttp = "com.github.sebruck" %% "opencensus-scala-akka-http" % "0.7.2"
 
   val sl4j = "org.slf4j" % "slf4j-api" % "2.0.11"
-  val sharedDeps = "com.google.cloud" % "google-cloud-shared-dependencies" % "3.23.0"
+  val sharedDeps = "com.google.cloud" % "google-cloud-shared-dependencies" % "3.24.0"
   val logback = "ch.qos.logback" % "logback-classic" % "1.4.14"
   val logbackCore = "ch.qos.logback" % "logback-core" % "1.4.14"
 
